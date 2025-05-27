bfd4ad6dd4caa224116107221b9ab42bb4a69c2a
mlendvaj <mihael.lendvaj@gmail.com>
2025-05-27 08:13:45 +0200

    Initial Commit

---
+++ Vrtko/VrtkoApp.swift
@@ -0,0 +1,17 @@
+//
+//  VrtkoApp.swift
+//  Vrtko
+//
+//  Created by Mihael Lendvaj on 27.05.2025..
+//
+
+import SwiftUI
+
+@main
+struct VrtkoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VrtkoTests/VrtkoTests.swift
@@ -0,0 +1,17 @@
+//
+//  VrtkoTests.swift
+//  VrtkoTests
+//
+//  Created by Mihael Lendvaj on 27.05.2025..
+//
+
+import Testing
+@testable import Vrtko
+
+struct VrtkoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
