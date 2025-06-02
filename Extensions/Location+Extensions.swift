fba46510885686b468f7901695bc7d4acdf1476e
mlendvaj <mihael.lendvaj@gmail.com>
2025-06-02 16:17:21 +0200

    Fix customMapPicker

--- Extensions/Location+Extensions.swift
+++ Extensions/Location+Extensions.swift
@@ -7,7 +7,12 @@
 
 import CoreLocation
 
-extension CLLocationCoordinate2D: Equatable {
+extension CLLocationCoordinate2D: Hashable {
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(latitude)
+        hasher.combine(longitude)
+    }
+    
     public static func == (lhs: CLLocationCoordinate2D, rhs: CLLocationCoordinate2D) -> Bool {
         lhs.latitude == rhs.latitude && lhs.longitude == rhs.longitude
     }
